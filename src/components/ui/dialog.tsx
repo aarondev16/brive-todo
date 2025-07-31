9f6a1b486c4b0ebd5053664af914e4e9f2913ec4
aarondev16 <aaronoso0704@gmail.com>
2025-07-30 22:39:58 -0600

    update dialog styles, dependencies, and gitignore

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -58,10 +58,7 @@ function DialogContent({
 			<DialogPrimitive.Content
 				data-slot="dialog-content"
 				className={cn(
-					"data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95" +
-						"data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)]" +
-						"translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border bg-background shadow-lg duration-200" +
-						"p-4 data-[state=closed]:animate-out data-[state=open]:animate-in sm:max-w-[500px]",
+					"data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border bg-background p-6 shadow-lg duration-200 data-[state=closed]:animate-out data-[state=open]:animate-in sm:max-w-lg",
 					className,
 				)}
 				{...props}
@@ -85,10 +82,7 @@ function DialogHeader({ className, ...props }: React.ComponentProps<"div">) {
 	return (
 		<div
 			data-slot="dialog-header"
-			className={cn(
-				"flex flex-col gap-2 rounded-t-lg border-b py-2 text-center sm:text-left",
-				className,
-			)}
+			className={cn("flex flex-col gap-2 text-center sm:text-left", className)}
 			{...props}
 		/>
 	);
@@ -99,7 +93,7 @@ function DialogFooter({ className, ...props }: React.ComponentProps<"div">) {
 		<div
 			data-slot="dialog-footer"
 			className={cn(
-				"flex flex-col-reverse gap-2 rounded-b-lg bg-white p-3 sm:flex-row sm:justify-end dark:bg-transparent",
+				"flex flex-col-reverse gap-2 sm:flex-row sm:justify-end",
 				className,
 			)}
 			{...props}
